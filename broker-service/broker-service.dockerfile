314130166a2fceec917b606189a9ee6cdf5cde97
z.nagashybayev <z.nagashybayev@andersenlab.com~>
2022-07-25 15:08:38 +0600

    Makefile, minor changes

--- broker-service/broker-service.dockerfile
+++ broker-service/broker-service.dockerfile
@@ -1,19 +1,8 @@
-#base of golang image
-FROM golang:1.18-alpine as builder
-
-RUN mkdir /app
-
-COPY . /app
-
-WORKDIR /app
-#cgo is desabled i will not use it
-RUN CGO_ENABLED=0 go build -o brokerApp ./cmd/api
-
 #smaller image
 FROM alpine:latest
 
 RUN mkdir /app
 
-COPY --from=builder /app/brokerApp /app
+COPY brokerApp /app
 
 CMD [ "/app/brokerApp" ]
--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 const webPort = "8001"
@@ -29,45 +24,4 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-}
-
-type Config struct {
-}
-
-func (app *Config) routes() http.Handler {
-	mux := chi.NewRouter()
-
-	// specify who is allowed to connect
-	mux.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
-
-	mux.Use(middleware.Heartbeat("/ping"))
-
-	mux.Post("/", app.Broker)
-
-	return mux
-}
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
-func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
-		Error:   false,
-		Message: "Hit the broker",
-	}
-
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
 }
